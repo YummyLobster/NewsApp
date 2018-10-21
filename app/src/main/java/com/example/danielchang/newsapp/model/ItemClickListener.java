843303c07616993a71acede82160d049eaec7d16
YummyLobster <dchanggcsula@gmail.com>
2018-10-21 03:13:22 -0700

    RecyclerView Done

---
+++ app/src/main/java/com/example/danielchang/newsapp/model/ItemClickListener.java
@@ -0,0 +1,7 @@
+package com.example.danielchang.newsapp.model;
+
+import android.view.View;
+
+public interface ItemClickListener {
+    void onClick(View view, int position, boolean isLongClick);
+}
